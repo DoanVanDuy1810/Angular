f8f93351aae771422e5cf4a3a01f4f76bf35790b
DoanVanDuy1810 <duydvpc03962@fpt.edu.vn>
2023-08-26 10:12:13 +0700

    NgFor and NgClass for ArrayOject

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,29 @@ export class HomeComponent implements OnInit{
   public name = 'Urban Legend China';
   public age = 25;
   public contries = ['ShangHai', 'Wuhan', 'Bejing', 'Chongqing', 'Guangdong']
+  public countries2 = [
+    {
+      name: 'ShangHai',
+      landArea: 675,
+      country: 'China'
+
+    },
+    {
+      name: 'New york',
+      landArea: 783,
+      country: 'USA'
+    },
+    {
+      name: 'TP.HCM',
+      landArea:  2095,
+      country: "VN"
+    },
+    {
+      name: 'Seoul',
+      landArea: 605,
+      country: 'Korea'
+    }
+  ];
 
   constructor(){}
 
